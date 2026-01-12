d47dcf625e164709674062664ea01d16d2081348
Rajesh.Muthusamy1 <rajesh.muthusamy@mallow-tech.com>
2026-01-12 16:50:22 +0530

    feat: update datasets

--- hooks/create-session-hook.js
+++ hooks/create-session-hook.js
@@ -5,14 +5,12 @@ module.exports = async ({ container, context }) => {
         console.log('Reusing existing session:', context.sessionId);
         return;
     }
-    const userUuid = process.env.USER_UUID;
-    console.log("User UUID:", userUuid);
 
     return new Promise((resolve, reject) => {
         request({
             url: `${process.env.CHATBOT_SERVER_URL}/api/v1/session`,
             method: "POST",
-            headers: { "auth-uuid": userUuid }
+            headers: { "Cookie": `${process.env.COOKIE}` }
         }, (err, res, body) => {
             if (err) return reject(err);
             try {
